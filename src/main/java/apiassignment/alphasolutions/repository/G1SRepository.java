339e9796a9ae3782f8051c8bc254a7dd53d71097
Zahaawii <Zahaawii@gmail.com>
2025-05-02 10:31:58 +0200

    adding classes and data

--- src/main/java/apiassignment/alphasolutions/repository/G1SRepository.java
+++ src/main/java/apiassignment/alphasolutions/repository/G1SRepository.java
@@ -1,7 +1,17 @@
 package apiassignment.alphasolutions.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class G1SRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public G1SRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+
 }
